7f0a4c0e6f95eea4c121bf00eb0dc5e4cbf0bdcc
KajetanMieloch <91525250+KajetanMieloch@users.noreply.github.com>
2023-08-25 15:40:52 +0200

    Update main.py
    Completed task of making that 5 shifts in a row is prefered. 
    
    Bug in witch after 5 shifts employee doesn't get a shift is now fixed
    
    Recurency is still buged.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from typing import List, Tuple
- 
+
+
+#Number of days in schedule
+days = 1
+
+
+
 # Employee class
 class Employee:
     def __init__(self, name: str, surename: str, possibleShifts: List[List[int]], descont: bool, coordinator: bool, contract: bool):
@@ -10,51 +16,68 @@ class Employee:
         self.coordinator = coordinator
         self.contract = contract
         self.offShifts = []
+        self.shiftsInRow = 0
 
 # Generate the schedule
 def generate_schedule(employees: list[Employee]) -> list[list[tuple[str, int]]]:
-    schedule = [[] for _ in range(7)]
-    num_shifts = {employee.name: 0 for employee in employees}
+    schedule = [[] for _ in range(days)]
+    
     # Generate the of days (range)
-    for day in range(3):
+    for day in range(days):
         # Generate the shifts (range)
         for shift in [1, 2, 3]:
             # Generate the employees (list comprehension)
             available_employees = [employee for employee in employees if shift in employee.possibleShifts[day] if shift not in employee.offShifts]
             # Generate the contract employees (list comprehension)
             contract_employees = [employee for employee in available_employees if employee.contract]
+            
+            contractEmployOnShift = False
+            
             # Assign the contract employees
             for employee in contract_employees:
                 schedule[day].append((employee.name, shift))
-                num_shifts[employee.name] += 1
                 available_employees.remove(employee)
-            # Sort the available employees by the number of shifts they have
-            #TODO change rules, that schedule will prioritize epmloyees to get 3-5 shifts in row
-            available_employees.sort(key=lambda employee: num_shifts[employee.name])
+                contractEmployOnShift = True
+            
+           
             coordinator = next((employee for employee in available_employees if employee.coordinator), None)
-            # Assign the coordinator (no more than one per shift)
+            
+            # Assign the coordinator (no more than one per shift)  
             if coordinator:
                 schedule[day].append((coordinator.name, shift))
-                num_shifts[coordinator.name] += 1
                 continue
-            # Assign the descont employee (at least one per shift)
-            descont_employee = next((employee for employee in available_employees if employee.descont), None)
-            if descont_employee:
-                schedule[day].append((descont_employee.name, shift))
-                num_shifts[descont_employee.name] += 1
-                available_employees.remove(descont_employee)
+            
+            #Assign the descont employee (at least one per shift)
+            descont_employee = [employee for employee in available_employees if employee.descont]
+            #Sort the list of descont employees by shiftsInRow
+            descont_employee.sort(key=lambda employee: employee.shiftsInRow, reverse=True)
+            if descont_employee is not None and not contractEmployOnShift:
+                schedule[day].append((descont_employee[0].name, shift))
+                available_employees.remove(descont_employee[0])
+                descont_employee[0].shiftsInRow += 1
+                #Number of shifts in row is reseted to 0
+                if descont_employee[0].shiftsInRow == 5:
+                    descont_employee[0].shiftsInRow = -1
+            
             # Assign the rest of the employees
+            available_employees.sort(key=lambda employee: employee.shiftsInRow, reverse=True)
+          
+
             while len([x for x in schedule[day] if x[1] == shift]) < 3:
                 if not available_employees:
                     break
                 employee = available_employees.pop(0)
                 schedule[day].append((employee.name, shift))
-                num_shifts[employee.name] += 1
-            # If there are not enough employees, generate the schedule again
+                employee.shiftsInRow += 1
+                #Number of shifts in row is reseted to 0
+                if employee.shiftsInRow == 5:
+                    employee.shiftsInRow = -1
+                
+            #If there is less than 2 employees on shift, then recurency is called
             if len([x for x in schedule[day] if x[1] == shift]) < 2:
-                #TODO Recuration is not working properly, need to be fixed
-                return generate_schedule(employees)
-        #At least 2 shifts between work
+                print("Recurency")
+                #TDOO: Recurency is called and the shift is changed.
+
         if day != 0:
             prevDaySchedule = [employee for employee in schedule][day-1]
             for n in range(len(prevDaySchedule)):
@@ -64,44 +87,39 @@ def generate_schedule(employees: list[Employee]) -> list[list[tuple[str, int]]]:
                             employee.offShifts.clear()
                             employee.offShifts.append(1)
                             employee.offShifts.append(2)
-                            print(employee.name, employee.offShifts)
                 elif prevDaySchedule[n][1] == 2:
                     for employee in employees:
                         if employee.name == prevDaySchedule[n][0]:
                             employee.offShifts.clear()
                             employee.offShifts.append(1)
-                            print(employee.name, employee.offShifts)
                 else:
                     for employee in employees:
                         if employee.name == prevDaySchedule[n][0]:
                             employee.offShifts.clear()
-                            print(employee.name, employee.offShifts)
 
 
     return schedule
  
 
-PJ = Employee("P", "J", [[2],[2],[2],[2],[2],[2],[2]], True,False,False)
-KM = Employee("K", "M", [[2],[3],[2],[2],[2],[1,2],[1,2]], False,False,False)
-KM1 = Employee("K", "M", [[3],[3],[3],[3],[3],[3],[3]], True,False,True)
-DS = Employee("D", "S", [[2,3],[3],[2,3],[2,3],[2,3],[1,2,3],[1,2,3]], True,False,False)
-MB = Employee("M", "B", [[2],[3],[2],[2],[2],[2,3],[2,3]], True,False,False)
-KT = Employee("K", "T", [[2],[2],[2],[2],[2],[2],[2]], True,False,True)
-MM = Employee("M", "M", [[2],[2],[2],[2],[2,3],[1,2,3],[1,2,3]], False,False,False)
-BT = Employee("B", "T", [[3],[3],[3],[3],[3],[3],[3]],False,False,False)
-KD = Employee("K", "D", [[2],[2],[3],[3],[3],[1],[1]],True,False,False)
-GW = Employee("G", "W", [[2],[2],[2],[3],[3],[1],[1]], True,False,False)
-JG = Employee("J", "G", [[2,3],[3],[2],[3],[2,3],[2,3],[1]], True,False,False)
-FJ = Employee("F", "J", [[1],[1],[1],[1],[1],[],[]], False,True,False)
-KC = Employee("K", "C", [[3],[2],[3],[2],[3],[3],[1]], True,False,False)
+PJ = Employee("Same2.0", "J", [[]], True,False,False)
+KM2 = Employee("Same1 - Brutusia", "M", [[1]], True,False,False)
+KM3 = Employee("Same2, weekend 1,2 - Gosia", "M", [[]], True,False,False)
+KM1 = Employee("Same2.2 - UmowaPraca", "M", [[2]], True,False,True)
+DS = Employee("Same3.0", "S", [[3]], True,False,False)
+MB = Employee("Same3.1", "B", [[3]], True,False,False)
+KC = Employee("Same3.3 - Kasia", "C", [[1]], True,False,False)
+BM = Employee("Same3.4 - Basia", "M", [[1]], True,False,False)
+KT = Employee("same3.2 - UmowaPraca", "T", [[3]], True,False,True)
+FJ = Employee("Koordynator", "J", [[]], False,True,False)
 
-employees = [PJ, KM, KM1, DS, MB, KT, MM, BT, KD, GW, JG, FJ, KC]
+
+employees = [PJ, KM1, DS, MB, KT, FJ, KC, BM, KM2, KM3]
 
 # Generate the schedule
 schedule = generate_schedule(employees)
 
 # Print the schedule
-for day in range(7):
+for day in range(days):
     print(f"Day {day + 1}:")
     for employee_name, shift in schedule[day]:
         print(f"  Shift {shift}: {employee_name}")
