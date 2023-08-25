4066d267992fbb31f342b6950b8ec524d3037e37
KajetanMieloch <91525250+KajetanMieloch@users.noreply.github.com>
2023-08-25 21:08:44 +0200

    Update main.py
    Bug destroyed #5

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from typing import List, Tuple
 
+
 #Number of days in schedule
 days = 3
+
+
 
 # Employee class
 class Employee:
@@ -41,6 +44,7 @@ def isNextShiftDescont(employees: list[Employee], day: int, shift: int) -> bool:
     descont_employee = [employee for employee in available_employees if employee.descont]
     if descont_employee:
         try:
+            #If descont employee has 2 possible shifts, then he is forced to be assigned on next shift
             descont_employee[0].doNotAssign = True
         except:
             pass
@@ -54,32 +58,23 @@ def generate_schedule(employees: list[Employee]) -> list[list[tuple[str, int]]]:
     
     # Generate the of days (range)
     for day in range(days):
-                
+        
+               
         nonDescontPriority = False
         
         #Set hadShift to False for all employees
         allEmployees = [employee for employee in employees]
         for employee in allEmployees:
             employee.hadShift = False
+            employee.doNotAssign = False
 
         # Generate the shifts (range)
         for shift in [1, 2, 3]:
-
+            
             # Get the available employees
             available_employees = availableEmployees(employees, day, shift)
 
-            # Generate the contract employees (list comprehension)
-            contract_employees = [employee for employee in available_employees if employee.contract]
-            
-            contractEmployOnShift = False
-            
-            # Assign the contract employees
-            for employee in contract_employees:
-                schedule[day].append((employee.name, shift))
-                available_employees.remove(employee)
-                contractEmployOnShift = True
-            
-           
+            # Get the coordinator
             coordinator = next((employee for employee in available_employees if employee.coordinator), None)
             
             #Get all descont employees
@@ -96,47 +91,57 @@ def generate_schedule(employees: list[Employee]) -> list[list[tuple[str, int]]]:
                 if len(descont_employee) < 7 and shift == 1:
                     nonDescontPriority = True
             
+            
+            # Generate the contract employees (list comprehension)
+            contract_employees = [employee for employee in available_employees if employee.contract]
+            
+            
+            # Assign the contract employees
+            for employee in contract_employees:
+                schedule[day].append((employee.name, shift))
+                available_employees.remove(employee)
+            
             #Assign the descont employee (at least one per shift)
             #Sort the list of descont employees by shiftsInRow
             descont_employee.sort(key=lambda employee: employee.shiftsInRow, reverse=True)
-            if descont_employee and not contractEmployOnShift:
+            if descont_employee:
                 schedule[day].append((descont_employee[0].name, shift))
                 available_employees.remove(descont_employee[0])
-                
+                descontAlreadyAssigned = True
                 shiftsInRow(descont_employee[0])
                 
                 
-            elif not descont_employee:                             
+            elif not descont_employee:                     
                 raise Exception("not enough descont employees on day " + str(day+1) + " shift " + str(shift))
             
             # Assign the rest of the employees
             available_employees.sort(key=lambda employee: employee.shiftsInRow, reverse=True)
      
             iterationOfNonDescontEmployee = 0
+            iterationOfDescontEmployee = 0
             
             while len([x for x in schedule[day] if x[1] == shift]) < 3:
-                if not available_employees:
-                    break
                 
                 iterationOfNonDescontEmployee += 1
+                iterationOfDescontEmployee += 1
                 
                 #Check if there is descont employee on next shift
                 if shift != 3:
                     if isNextShiftDescont(employees, day, shift):
                         nonDescontPriority = False
-                    
+        
                 
                 #If there is nonDescontPriority, then non descont employee is assigned first
                 if nonDescontPriority and iterationOfNonDescontEmployee != 3 and shift != 3:
                     for employee in available_employees:
-                        if employee.descont == False:
+                        if employee.descont == False and employee.doNotAssign == False:
                             schedule[day].append((employee.name, shift))
                             available_employees.remove(employee)
                             shiftsInRow(employee)
                             break
                 #If there is no nonDescontPriority, then descont employee is assigned first
                 #but if employee has doNotAssign set to True, then he is not assigned
-                elif iterationOfNonDescontEmployee != 3 and shift != 3:
+                elif iterationOfDescontEmployee != 3 and shift != 3:
                     for employee in available_employees:
                         if employee.doNotAssign == False:
                             schedule[day].append((employee.name, shift))
@@ -146,48 +151,73 @@ def generate_schedule(employees: list[Employee]) -> list[list[tuple[str, int]]]:
                 
                 #Else, employee is assigned by order
                 else:
-                    employee = available_employees.pop(0)
-                    schedule[day].append((employee.name, shift))
-                    shiftsInRow(employee)
-                
+                    if employee.doNotAssign == False:                        
+                        try:
+                            employee = available_employees.pop(0)
+                            schedule[day].append((employee.name, shift))
+                            shiftsInRow(employee)
+                        except:
+                            #If there is no more employees to assign, but there are at least 2 employees on shift,
+                            #then go to next shift
+                            if len([x for x in schedule[day] if x[1] == shift]) >= 2:
+                                break
+                            #If there is less than 2 employees on shift, then recurency is called
+                            else:
+                                #TODO Program a recurency, that will figure out what to do if there is no more employees to assign
+                                raise Exception("Not enough employees on shift")
+                    else:
+                        try:
+                            employee = available_employees.pop(1)
+                            schedule[day].append((employee.name, shift))
+                            shiftsInRow(employee)
+                        except:
+                            #If there is no more employees to assign, but there are at least 2 employees on shift,
+                            #then go to next shift
+                            if len([x for x in schedule[day] if x[1] == shift]) >= 2:
+                                break
+                            #If there is less than 2 employees on shift, then recurency is called
+                            else:
+                                #TODO Program a recurency, that will figure out what to do if there is no more employees to assign
+                                raise Exception("Not enough employees on shift")
+                            
+            #TODO Program a recurency, that will figure out what to do if there is no more employees to assign
             #If there is less than 2 employees on shift, then recurency is called
             if len([x for x in schedule[day] if x[1] == shift]) < 2:
                 raise Exception("Not enough employees on shift")
-                #TDOO: Recurency is called and the shift is changed.
 
-        if day != 0:
-            prevDaySchedule = [employee for employee in schedule][day-1]
-            for n in range(len(prevDaySchedule)):
-                if prevDaySchedule[n][1] == 3:
-                    for employee in employees:
-                        if employee.name == prevDaySchedule[n][0]:
-                            employee.offShifts.clear()
-                            employee.offShifts.append(1)
-                            employee.offShifts.append(2)
-                elif prevDaySchedule[n][1] == 2:
-                    for employee in employees:
-                        if employee.name == prevDaySchedule[n][0]:
-                            employee.offShifts.clear()
-                            employee.offShifts.append(1)
-                else:
-                    for employee in employees:
-                        if employee.name == prevDaySchedule[n][0]:
-                            employee.offShifts.clear()
+        prevDaySchedule = [employee for employee in schedule][day]
+        for n in range(len(prevDaySchedule)):
+            if prevDaySchedule[n][1] == 3:
+                for employee in employees:
+                    if employee.name == prevDaySchedule[n][0]:
+                        employee.offShifts.clear()
+                        employee.offShifts.append(1)
+                        employee.offShifts.append(2)
+            elif prevDaySchedule[n][1] == 2:
+                for employee in employees:
+                    if employee.name == prevDaySchedule[n][0]:
+                        employee.offShifts.clear()
+                        employee.offShifts.append(1)
+            else:
+                for employee in employees:
+                    if employee.name == prevDaySchedule[n][0]:
+                        employee.offShifts.clear()
 
 
     return schedule
  
 
 PJ = Employee("Maryja", "J", [[2],[2],[2]], True,False,False)
-KM2 = Employee("Brutusia", "M", [[2],[2],[2]], False,False,False)
-KM3 = Employee("Gosia", "M", [[2],[2],[2,3]], True,False,False)
+KM2 = Employee("Brutusia", "M", [[2],[2],[]], False,False,False)
+KM3 = Employee("Gosia", "M", [[3],[2],[2,3]], True,False,False)
 KM1 = Employee("UmowaPraca - Krystian", "M", [[],[],[]], False,False,True)
 DS = Employee("Kajetan", "S", [[3],[3],[3]], False,False,False)
-MB = Employee("Maks", "B", [[],[2],[2,3]], False,False,False)
-KC = Employee("Kasia", "C", [[3],[3],[]], True,False,False)
-BM = Employee("Basia", "M", [[3],[3],[3]], False,False,False)
+MB = Employee("Maks", "B", [[2],[],[2,3]], False,False,False)
+KC = Employee("Kasia", "C", [[],[3],[]], True,False,False)
+BM = Employee("Basia", "M", [[3],[],[3]], False,False,False)
 KT = Employee("UmowaPraca - Kacper", "T", [[],[],[]], False,False,True)
 FJ = Employee("Koordynator", "J", [[1],[1],[1]], False,True,False)
+
 
 
 employees = [PJ, KM1, DS, MB, KT, FJ, KC, BM, KM2, KM3]
