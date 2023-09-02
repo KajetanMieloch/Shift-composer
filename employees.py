428baf1d3215990474c60f30ddb43dc5451956eb
KajetanMieloch <91525250+KajetanMieloch@users.noreply.github.com>
2023-09-02 21:56:01 +0200

    Update employees.py

--- employees.py
+++ employees.py
@@ -1,34 +1,48 @@
 from typing import List
+import json
+
 # Employee class
 class Employee:
-    def __init__(self, name: str, surename: str, id: int,shiftsInRow :int, possibleShifts: List[List[int]], descont: bool, coordinator: bool, contract: bool,):
+    def __init__(self, name: str, surename: str, id: int, day0 :int, possibleShifts: List[List[int]], descont: bool, coordinator: bool, contract: bool,):
         self.name = name
         self.surename = surename
         self.id = id
+        self.day0 = day0
         self.possibleShifts = possibleShifts   
         self.descont = descont
         self.coordinator = coordinator
         self.contract = contract
         self.offShifts = []
-        self.shiftsInRow = shiftsInRow
+        self.shiftsInRow = 0
         self.hadShift = False
         self.doNotAssign = False
         self.hoursInTotal = 0
         self.workedDays = []
+        
+        self.setOffShiftsBasedOnDay0()
+    
+    def setOffShiftsBasedOnDay0(self):
+        if self.day0 == 2:
+            self.offShifts = [1]
+        elif self.day0 == 3:
+            self.offShifts = [1,2]
+
+with open('employees.json', 'r') as f:
+    data = json.load(f)
 
 #Define employees
-def defineEmployees(n = 0):
-    FJ = Employee("Filip", "J",1,n, [[1],[1],[1],[1],[1],[],[],[1],[1],[1],[1],[1],[],[],[1],[1],[1],[1],[1],[],[],[1],[1],[1],[1],[1],[],[],[1],[1],[1]], False,True,False)
-    KM = Employee("Kacper", "M",2,n, [[2],[2],[2],[],[],[2],[2],[2],[2],[2],[],[],[2],[2],[2],[2],[2],[],[],[2],[2],[2],[2],[2],[],[],[2],[2],[2],[2],[2]], True,False,True)
-    MB = Employee("Maks", "B",3,n, [[2],[2],[3],[2,3],[2,3],[1,2],[1,2],[],[],[],[],[],[1],[3],[3],[3],[2,3],[2],[2,3],[2],[2,3],[3],[],[],[],[],[2],[3],[2],[2],[2,3]], True,False,False)
-    KT = Employee("Krystian", "T",4,n, [[3],[],[],[3],[3],[3],[3],[3],[],[],[3],[3],[3],[3],[3],[],[],[3],[3],[3],[3],[3],[],[],[3],[3],[3],[3],[3],[],[]], True,False,True)
-    KD = Employee("Kamil", "M",5,n, [[3],[2],[3],[2],[],[],[],[],[],[],[],[],[],[2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[],[],[],[],[]], True,False,False)
-    GW = Employee("Grzegorz", "W",6,n, [[],[],[3],[2],[2],[1],[1],[2],[],[],[2],[2],[1],[1],[2],[2],[],[],[2],[1],[1],[],[],[],[2],[2],[1],[1],[2],[2],[2]], True,False,False)
-    KM2 = Employee("Kajetan", "M",7,n, [[2,3],[2],[2],[2,3],[2,3],[1,2],[1,2],[2],[2],[2],[2],[2],[1,2],[1,2],[2],[2],[2],[2],[2,3],[1,2],[1,2],[2,3],[2,3],[2,3],[2,3],[2,3],[1,2,3],[1,2,3],[2,3],[2,3],[2,3]], False,False,False)
-    PJ = Employee("Piotr", "J",8,n, [[3],[3],[3],[2,3],[2,3],[3],[3],[3],[3],[2,3],[3],[3],[3],[3],[3],[3],[2,3],[2],[2,3],[2],[],[],[],[],[],[],[],[3],[2],[2],[2,3]], True,False,False)
-    KC = Employee("Kasia", "C",9,n, [[3],[3],[],[3],[],[],[],[3],[2],[3],[3],[2],[3],[3],[],[3],[3],[],[3],[],[],[],[3],[2],[3],[3],[2],[3],[3],[],[3]], True,False,False)
-    DS = Employee("Damian", "S",10,n, [[2,3],[2,3],[3],[2,3],[2,3],[1,2,3],[1,2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[1,2,3],[1,2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[1,2,3],[1,2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[1,2,3],[1,2,3],[2,3],[2,3],[2,3]], True,False,False)
-    JG = Employee("Jakub", "G",9,n, [[2,3],[2,3],[3],[2,3],[2,3],[1,2,3],[1,2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[1,2,3],[1,2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[1,2,3],[1,2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[1,2,3],[1,2,3],[2,3],[2,3],[2,3]], True,False,False)
-    MM = Employee("Mateusz", "M",12,n, [[2,3],[2],[2],[2,3],[],[1],[2],[3],[2,3],[2,3],[],[2],[],[],[],[2],[2],[2],[2],[3],[2,3],[2,3],[2,3],[2,3],[2,3],[1],[1],[],[2],[2],[2]], False,False,False)
-        
-    return [FJ, KM, MB, KT, KD, GW, KM2, PJ, KC, DS, JG, MM]
+def defineEmployees():
+    employees = []
+    for employeeData in data:
+        employee = Employee(
+            employeeData['first_name'],
+            employeeData['last_name'],
+            employeeData['id'],
+            employeeData['day0'],
+            employeeData['schedule'],
+            employeeData['is_manager'],
+            employeeData['is_full_time'],
+            employeeData['is_part_time']
+        )
+        employees.append(employee)
+    return employees
